5434eb0a862f4a01688158274759a1addd44e6cf
MayMay <maylis.baschet@gmail.com>
2019-08-22 11:31:44 +0200

    check data before loading

--- JsonAPI.py
+++ JsonAPI.py
@@ -1,22 +1,35 @@
 import requests
 import json
+import os
 
 from models.config import *
 
+def check_data():
+    path = os.getcwd()
+    if os.path.isdir(path + "/data"):
+        return True
+    else :
+        os.mkdir(path + "/data")
+        return False
 
 def get_categories():
-    url = 'https://fr.openfoodfacts.org/categories.json'
-    data = requests.get(url).json()
-    with open('data/categories.json', 'w') as f:
-        f.write(json.dumps(data, indent=4))
+    if check_data() == False :
 
-def get_products(categorie):
-    url = 'https://world.openfoodfacts.org/cgi/search.pl?search_tag=categories&search_terms='\
-            + categorie + '&page_size=1&json=1'
-    data = requests.get(url).json()
-    file = 'data/Products_' + categorie + '.json'
-    with open(file, 'w') as f:
-        f.write(json.dumps(data, indent=4))
+        # load categories
+        url = 'https://fr.openfoodfacts.org/categories.json'
+        data = requests.get(url).json()
+        with open('data/categories.json', 'w') as f:
+            f.write(json.dumps(data, indent=4))
+
+        # load products
+        for name, urlname in CATEGORIES.items():
+            url = 'https://world.openfoodfacts.org/cgi/search.pl?search_tag=categories&search_terms='\
+                    + urlname + '&json=1'
+            data = requests.get(url).json()
+            file = 'data/Products_' + name + '.json'
+            with open(file, 'w') as f:
+                f.write(json.dumps(data, indent=4))
+
 
 
 if __name__ == "__main__":
